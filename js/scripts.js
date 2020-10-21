ce6eeb1e4505be7fb5027614893ac1a7ed025e74
Duc <phamtrungduc.cd2@gmail.com>
2020-10-21 12:28:56 +0800

    Update screenshots and description

--- js/scripts.js
+++ js/scripts.js
@@ -20,17 +20,6 @@
 		hidePreloader();
 	});
 
-	
-	/* Navbar Scripts */
-	// jQuery to collapse the navbar on scroll
-    $(window).on('scroll load', function() {
-		if ($(".navbar").offset().top > 20) {
-			$(".fixed-top").addClass("top-nav-collapse");
-		} else {
-			$(".fixed-top").removeClass("top-nav-collapse");
-		}
-    });
-
 	// jQuery for page scrolling feature - requires jQuery Easing plugin
 	$(function() {
 		$(document).on('click', 'a.page-scroll', function(event) {
@@ -56,7 +45,7 @@
 		// An array of phrases to rotate are created based on this separator. Change it if you wish to separate the phrases differently (e.g. So Simple | Very Doge | Much Wow | Such Cool).
 		separator: ",",
 		// The delay between the changing of each phrase in milliseconds.
-		speed: 2500,
+		speed: 3000,
 		complete: function () {
 			// Called after the entrance animation is executed.
 		}
@@ -92,7 +81,7 @@
     /* Image Slider - Swiper */
     var imageSlider = new Swiper('.image-slider', {
         autoplay: {
-            delay: 2000,
+            delay: 4000,
             disableOnInteraction: false
 		},
         loop: false,
